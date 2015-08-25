b379883db6e3530109726b9d835a1bae018c92cc
hazmatzo <hazmatzo@users.noreply.github.com>
2015-08-24 22:20:36 -0700

    Calculating whether points are all on the same line.

--- other/points_on_a_line.rb
+++ other/points_on_a_line.rb
@@ -3,41 +3,37 @@
 
 # returns true if all the points in the array lie on the same line
 
-# collinear([[1,2], [2,1]]) => true
-
-# collinear([ [1,2], [2,3], [3,4], [4,5] ]) => true
-
-# collinear([[1,2], [2,1], [4,0]]]) => false
-
 def is_collinear(params)
-    if params.length == ( 1 | 0 )
-        return false
-   elsif params.length == 2
-       return true
-   elsif is_horizontal_line(params)
-       return true
-   elsif is_vertical_line(params)
-       return true
-   elsif is_diagonal_line(params)
-       return true
-   else
-       return false        
-   end      
+  if params.length == ( 1 | 0 )
+    return false
+  elsif params.length == 2
+    return true
+  elsif is_horizontal_line(params)
+    return true
+  elsif is_vertical_line(params)
+    return true
+  elsif is_diagonal_line(params)
+    return true
+  else
+    return false        
+  end      
 end
 
 # all y's the same
-def is_horizontal(params)
-    is_horizontal = true
-    first_y = params.first.last
-    params.each do |n1, n2|
-        if first_y != n2
-            is_horizontal = false
-        end
-    end 
+def is_horizontal_line(params)
+  is_horizontal = true
+  first_y = params.first.last
+  params.each do |n1, n2|
+    if first_y != n2
+      is_horizontal = false
+    end
+  end
+
+  is_horizontal 
 end
 
 # all x's the same
-def is_vertical(params)
+def is_vertical_line(params)
   is_vertical = true
   first_x = params.first.first
   params.each do |n1, n2|
@@ -45,37 +41,40 @@ def is_vertical(params)
       is_vertical = false
     end
   end
+
+  is_vertical
 end
 
 # y = mx + b
-# m = slope = (y2 - y1) / (x2 - x1)
-# b = y-intercept
-
-# a, b, c
-# ab bc
-
-
-
 def is_diagonal_line(params)
-   still_a_line = true
-   # params.each_slice(2) { |a,b| p [a, b] }
-   
-   params.each_slice(3) do |n1, n2, n3|
-      if !still_a_line
-          return false
-      else
-          if !(y_intercept(n1, n2) == y_intercept(n2, n3)) && (slope(n1, n2) == slope(n2, n3))
-              still_a_line = false
-          end
-      end
-   end
-   still_a_line
+  is_diagonal = true
+  
+  n = params.length - 3
+  (0..(n)).each do |n|
+    if !(slope(params[n], params[n+1]) == slope(params[n+1], params[n+2]))
+      is_diagonal = false
+    end
+  end
+
+  is_diagonal
 end
 
-def y_intercept(a, b)
-    
-end
+# # b = y - mx
+# def y_intercept(a, b)
+#   a[1] - (slope(a, b) * a[0])
+# end
 
+# m = slope = (y2 - y1) / (x2 - x1)
 def slope(a, b)
-
+  (a[1] - b[1]).to_f / (a[0] - b[0]).to_f
 end
+
+
+first_test = is_collinear([[1,2], [2,1]])  # => true
+puts "is_collinear([[1,2], [2,1]]) #{first_test}"
+
+second_test = is_collinear([ [1,2], [2,3], [3,4], [4,5] ])
+puts "is_collinear([ [1,2], [2,3], [3,4], [4,5] ]) #{second_test}" # => true
+
+third_test = is_collinear([[1,2], [2,1], [4,0]])
+puts "is_collinear([[1,2], [2,1], [4,0]]]) #{third_test}" # => false
